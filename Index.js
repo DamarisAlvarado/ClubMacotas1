23a36d6bf24f72935cf563e574f9b91e766048bb
DamarisAlvarado <94861147+DamarisAlvarado@users.noreply.github.com>
2023-03-27 03:53:45 -0600

    primerv

---
+++ Index.js
@@ -0,0 +1,45 @@
+
+
+function entrar(){
+    window.location="Login.html"
+}
+function registrar(){
+    window.location="Registrar.html"
+}
+function irmenu(){
+    window.location="menu.html"
+}
+function cerarsesion(){
+    window.location="index.html"
+}
+function Perfil(){
+    window.location="Perfil.html"
+}
+
+function regresar(){
+    window.location="menu.html"
+}
+function contacto(){
+    window.location="contactos.html"
+}
+function enfermedades(){
+    window.location="/enfermedades.html"
+}
+function cuidados(){
+    window.location="cuidados.html"
+}
+function alimentacion(){
+    window.location="Alimentacion.html"
+}
+function configuracion(){
+    window.location="configuracion.html";
+}
+function contactar(){
+    window.location="Contacto-sugerencias.html";
+}
+
+
+
+
+
+
